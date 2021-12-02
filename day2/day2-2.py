31a4866dceac470bf65eb7129c7dcabaafb3303f
Max Pochet <max.pochet@orange.fr>
2021-12-02 21:22:16 +0100

    jour 2

--- day2/day2-2.py
+++ day2/day2-2.py
@@ -1,12 +1,9 @@
 from typing import List
-
-
 liste = open('./day2/input.txt','r').read().split('\n')
 taille = len(liste)
 i=0
 horizontal = 0
 depth = 0
-
 listey = []
 
 while i < taille:
@@ -16,20 +13,10 @@ while i < taille:
     if mouvement == "forward":
         horizontal = horizontal + int(avancé)
         y = depth*int(avancé)
-        listey.append(y)
-        
-    
+        listey.append(y)    
     if mouvement == "up":
         depth = depth - int(avancé)
-
     if mouvement == "down":
         depth = depth + int(avancé)
-
     i=i+1
-print(sum(listey))
-print("y : ",y)
-somme=horizontal*sum(listey)
-print("horizontal ",horizontal)
-print("depth ", depth)
-print("somme ",somme)
-print(taille)
+print(horizontal*sum(listey))
